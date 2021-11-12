66b21eaae8ff0e621b489daf6fbe011c1b2b57d7
Raeez98 <raeezmohammed97@gmail.com>
2021-11-12 10:46:32 +0530

    counter set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[count,setCount]=useState(0);
+  
   return (
     <div className="App">
-      <h1>Hello World</h1>
+     <div className="count_button"
+     onClick={
+      ()=>{
+        setCount(count-1);
+      }
+    }
+    >
+       -
+     </div>
+     <div className="count_value">
+       {count}
+
+     </div>
+     <div className="count_button"
+     onClick={
+       ()=>{
+         setCount(count+1);
+       }
+     }
+     >
+     +
+     </div>
+
 
       </div>
-  };
+  );
 } 
       
 
